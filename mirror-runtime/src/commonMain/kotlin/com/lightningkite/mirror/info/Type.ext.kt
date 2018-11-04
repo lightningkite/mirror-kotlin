d5812baac070d835484b8e2072014b652d050bbe
Joseph Ivie <josephivie@gmail.com>
2018-11-04 00:10:24 -0600

    Stricter type result

--- mirror-runtime/src/commonMain/kotlin/com/lightningkite/mirror/info/Type.ext.kt
+++ mirror-runtime/src/commonMain/kotlin/com/lightningkite/mirror/info/Type.ext.kt
@@ -1,4 +1,6 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.lightningkite.mirror.info
 
-val <T : Any> Type<T>.list get() = Type(List::class, listOf(TypeProjection(this)))
-val <T : Any> Type<T>.stringMap get() = Type(Map::class, listOf(TypeProjection(String::class.type), TypeProjection(this)))
+val <T : Any> Type<T>.list get() = Type(List::class, listOf(TypeProjection(this))) as Type<List<T>>
+val <T : Any> Type<T>.stringMap get() = Type(Map::class, listOf(TypeProjection(String::class.type), TypeProjection(this))) as Type<Map<String, T>>
