66c67823c8595d946841795d908cc423970ac6ae
Joseph Ivie <josephivie@gmail.com>
2018-12-14 14:57:10 -0700

    Things build and publish at least

--- mirror-runtime/src/commonTest/kotlin/reflected/TestRegistry.kt
+++ mirror-runtime/src/commonTest/kotlin/reflected/TestRegistry.kt
@@ -6,12 +6,12 @@ import kotlin.reflect.KClass
 
 @SharedImmutable
 val TestRegistry = ClassInfoRegistry(
+    com.lightningkite.mirror.serialization.json.SimpleJSONTestPostClassInfo,
+    com.lightningkite.mirror.serialization.json.SimpleJSONTestTestEnumClassInfo,
     com.lightningkite.mirror.info.TestInterfaceClassInfo,
     com.lightningkite.mirror.info.TestParentClassInfo,
     com.lightningkite.mirror.info.TestAnotherInterfaceClassInfo,
     com.lightningkite.mirror.info.TestChildClassInfo,
-    com.lightningkite.mirror.serialization.json.SimpleJSONTestPostClassInfo,
-    com.lightningkite.mirror.serialization.json.SimpleJSONTestTestEnumClassInfo,
     com.lightningkite.lokalize.TimeStampClassInfo,
     com.lightningkite.recktangle.PointClassInfo
 )
