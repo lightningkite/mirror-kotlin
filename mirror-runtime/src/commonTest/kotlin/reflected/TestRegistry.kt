475fbe954c934de3edb78fc86a5bc0f327f8ed86
Joseph Ivie <josephivie@gmail.com>
2019-01-17 21:51:23 -0700

    Fixed issues

--- mirror-runtime/src/commonTest/kotlin/reflected/TestRegistry.kt
+++ mirror-runtime/src/commonTest/kotlin/reflected/TestRegistry.kt
@@ -6,11 +6,12 @@ import kotlin.reflect.KClass
 
 @SharedImmutable
 val TestRegistry = ClassInfoRegistry(
+        com.lightningkite.lokalize.time.TimeStampClassInfo,
     com.lightningkite.recktangle.PointClassInfo,
+        com.lightningkite.mirror.serialization.json.SimpleJSONTestPostClassInfo,
+        com.lightningkite.mirror.serialization.json.SimpleJSONTestTestEnumClassInfo,
     com.lightningkite.mirror.info.TestInterfaceClassInfo,
     com.lightningkite.mirror.info.TestParentClassInfo,
     com.lightningkite.mirror.info.TestAnotherInterfaceClassInfo,
-    com.lightningkite.mirror.info.TestChildClassInfo,
-    com.lightningkite.mirror.serialization.json.SimpleJSONTestPostClassInfo,
-    com.lightningkite.mirror.serialization.json.SimpleJSONTestTestEnumClassInfo
+        com.lightningkite.mirror.info.TestChildClassInfo
 )
